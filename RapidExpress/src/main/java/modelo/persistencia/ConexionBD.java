feb78870838d922b84613bbfc3111a0d2e2dcdde
Sergio Andres Aparicio Galvis <247629487+Sergioandresaparicio@users.noreply.github.com>
2026-08-29 15:29:56 -0500

    se creo Operaciones y conexionbd

--- RapidExpress/src/main/java/modelo/persistencia/ConexionBD.java
+++ RapidExpress/src/main/java/modelo/persistencia/ConexionBD.java
@@ -4,10 +4,34 @@
  */
 package modelo.persistencia;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author sergi
  */
-public class ConexionBD {
+public abstract class ConexionBD {
+    private static String url= "jdbc:Mysql://***REMOVED-HOST***:3306/rapidexpress_db";
+    private static String user = "root";
+    private static String password = "***REMOVED-CREDENTIAL***";
     
+    public static Connection con = null;
+    
+    public static Connection MySQLConnection(){
+        con = null;
+        try {
+            con = DriverManager.getConnection(url,user,password);
+            
+            if(con != null){
+                DatabaseMetaData meta = con.getMetaData();
+                System.out.println("Base de datos conectada: "+meta.getDriverName());
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error al conectar la BD: "+ex.getMessage());
+        }
+        return con;
+    }
 }
