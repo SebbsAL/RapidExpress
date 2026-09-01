257f5d6a3c5007d65a14f2b8c17c696545e2c630
Jhon Reyes <jhonhenryreyesperezstudio@gmail.com>
2026-09-01 10:49:18 -0500

    Revert "Test"

--- RapidExpress/src/main/java/controlador/ControladorReportes.java
+++ RapidExpress/src/main/java/controlador/ControladorReportes.java
@@ -3,30 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package controlador;
-import modelo.servicios.ServicioReportes;
-import java.util.Date;
+
 /**
  *
- * @author Sebastian
+ * @author sergi
  */
 public class ControladorReportes {
-    private static final String USUARIO_SISTEMA = "SISTEMA";
-    private ServicioReportes servicioReportes;
-    private ControladorAuditoria controladorAuditoria;
-    public ControladorReportes(ServicioReportes servicioReportes, ControladorAuditoria controladorAuditoria) {
-        this.servicioReportes = servicioReportes;
-        this.controladorAuditoria = controladorAuditoria;
-    }
-    public void generarReporteEntregasPorConductor(Date fechaInicio, Date fechaFin){
-        servicioReportes.obtenerReporteEntregasPorConductor(fechaInicio, fechaFin);
-        controladorAuditoria.registrar("REPORTES", "REPORTE_ENTREGAS",
-            "Reporte de entregas por conductor generado desde " + fechaInicio + " hasta " + fechaFin,
-            USUARIO_SISTEMA);
-    }
-    public void generarHistorialRutasVehiculo(String placa){
-        servicioReportes.obtenerHistorialRutasVehiculo(placa);
-        controladorAuditoria.registrar("REPORTES", "REPORTE_HISTORIAL_RUTAS",
-            "Historial de rutas del vehículo " + placa + " generado",
-            USUARIO_SISTEMA);
-    }
+    
 }
---
+++ RapidExpress/src/main/java/vista/RapidExpress.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vista;
+
+/**
+ *
+ * @author sergi
+ */
+public class RapidExpress {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
