3a60cbedd61b0c2062aad448a241a56824bf391c
SebbsAL <sebbs0610@gmail.com>
2026-08-31 23:40:14 -0500

    Feat: Drivers View and fixed author

--- RapidExpress/src/main/java/modelo/vistas/UtilidadConsola.java
+++ RapidExpress/src/main/java/modelo/vistas/UtilidadConsola.java
@@ -6,7 +6,7 @@ package vistas;
 import java.util.Scanner;
 /**
  * Utilidad para manejo de entrada por consola
- * @author RapidExpress
+ * @author Sebastian
  */
 public class UtilidadConsola {
     private static Scanner scanner = new Scanner(System.in);
