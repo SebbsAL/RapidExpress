ccbcb45ed7ee4ed2a91d5c6effbdcf0fdcfad8c3
JhonHenryReyesPerez <jhonhenryreyesperezstudio@gmail.com>
2026-09-02 17:37:47 -0500

    style: reformat SQL queries and organize imports across persistence layer

--- RapidExpress/src/main/java/modelo/persistencia/DaoVehiculos.java
+++ RapidExpress/src/main/java/modelo/persistencia/DaoVehiculos.java
@@ -1,5 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package modelo.persistencia;
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 /**
  *
- * @author sergi
+ * @author sergi 
  */
 public class DaoVehiculos {
     public void insertar(Vehiculos vehiculo) {
